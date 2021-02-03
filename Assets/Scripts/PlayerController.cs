e7c95ac505c6863f09b0002e74b451aba7c23cba
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-02-03 12:28:04 -0700

    Added Tutorial Steps 1-1.4

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float turnSpeed;
+    public float speed = 5.0f;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
